8813d755b85eca41bf5ce5099eca6791efd1be5f
llbllbllbllb <295319725@qq.com>
2020-05-12 20:38:43 -0400

    experiment on recharts

--- book-keeper-front-end/src/component/DataVisualization.js
+++ book-keeper-front-end/src/component/DataVisualization.js
@@ -2,12 +2,58 @@ import React from 'react';
 
 import {Container, Row, Col, Card, Form, Button, Nav, CardColumns} from 'react-bootstrap';
 
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
+const data = [
+    {
+      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    },
+    {
+      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    },
+    {
+      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    },
+    {
+      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    },
+    {
+      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
+  ];
 
 
 function DataVisualization(){
 
     return(
-        <h1>Data Visualization Page</h1>
+        <div>
+            <h1>Data Visualization</h1>
+            <LineChart
+                width={600}
+                height={400}
+                data={data}
+                margin={{
+                top: 5, right: 30, left: 20, bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="amt" stroke="#000000" />
+            </LineChart>
+
+        </div>
+
     );
 
 }

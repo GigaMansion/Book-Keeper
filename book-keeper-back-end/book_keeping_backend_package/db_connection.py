eaa9aca4c2888b2f53a516ed02651b9a58b77636
llbllbllbllb <295319725@qq.com>
2020-05-21 14:14:08 -0400

    db init

---
+++ book-keeper-back-end/book_keeping_backend_package/db_connection.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  passwd="yourpassword"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE book_keeper_DB")

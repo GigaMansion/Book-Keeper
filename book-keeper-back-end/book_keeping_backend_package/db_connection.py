53132dccf6b15882bdd3bf16c5d6c3efdcbdad5b
llbllbllbllb <295319725@qq.com>
2020-05-21 14:46:42 -0400

    db connect

--- book-keeper-back-end/book_keeping_backend_package/db_connection.py
+++ book-keeper-back-end/book_keeping_backend_package/db_connection.py
@@ -4,7 +4,7 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="yourusername",
   passwd="yourpassword",
-  database="CREATE DATABASE book_keeper_DB"
+  database="book_keeper_DB"
 )
 
 mycursor = mydb.cursor()

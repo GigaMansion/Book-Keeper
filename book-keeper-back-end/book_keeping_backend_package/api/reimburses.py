c0b8b4188c46c8d62987c8622d28813168000a72
wilsonwang881 <wilson.wang@wilsonwang.org>
2020-05-25 09:40:50 -0500

    Added api folder

---
+++ book-keeper-back-end/book_keeping_backend_package/api/reimburses.py
@@ -0,0 +1,20 @@
+from book_keeping_backend_package.api import bp
+
+@bp.route('/new_reimburse', methods=['POST'])
+def new_reimburse():
+    pass
+
+
+@bp.route('/cancel_reimburse', methods=['POST'])
+def cancel_reimburse():
+    pass
+
+
+@bp.route('/process_reimburse', methods=['POST'])
+def process_reimburse():
+    pass
+
+
+@bp.route('/see_reimburse', methods=['POST'])
+def see_reimburse():
+    pass

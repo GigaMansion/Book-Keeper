aa25b89fcf359a0118a5dc78eb9bfe4bc77bec72
WilsonWang881 <Wilson.Wang@WilsonWang.org>
2020-07-18 15:05:21 -0500

    changed the deploymeny script

--- book-keeper-back-end/deployment.sh
+++ book-keeper-back-end/deployment.sh
@@ -1,36 +1,34 @@
+# remove any existing docker installations
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
 sudo apt update
 
-# install required software on the Linux machine
-# with anwsering 'yes' to all questions asking for disk usage permission
-sudo apt install -y python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools nginx
+# allow apt to use repositories over HTTPS
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 
-# install the Python3 virtualenv environment
-sudo apt install python3-venv
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# clone the code to the root directory
-git clone https://github.com/GigaMansion/Book-Keeper.git
+# Verify the key with fingerprint
+# 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
+sudo apt-key fingerprint 0EBFCD88
 
-# change directory to the backend directory
-cd Book-Keeper/book-keeper-back-end
+# Add the 'stable' repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
-# create a virtualenv environment in the backend directory
-python3 venv venv
+sudo apt-get update
 
-# activate the virtualenv environment
-source venv/bin/activate
+# install docker engine
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-# install the packages from requirements.txt
-pip3 install -r requirements.txt
-
-# deactivate the virtual environemtn
-deactivate
-
-# move the system unit file to the systemctl folder
-sudo cp book-keeper-back-end.service /etc/systemd/system/book-keeper-back-end.service
-
-# start the system service
-sudo systemctl start book-keeper-back-end
-
-# enable the service so that it starts when the machine boots
-sudo systemctl enable book-keeper-back-end
-
+# start docker containers
+docker-compose build
+docker-compose up

373e534b4d386bf7c98bd7a3482844993b506485
WilsonWang881 <Wilson.Wang@WilsonWang.org>
2020-07-17 19:35:38 -0500

    working on routes

--- book-keeper-back-end/Backend/book_keeping_backend_package/auth/tokens.py
+++ book-keeper-back-end/Backend/book_keeping_backend_package/auth/tokens.py
@@ -1,6 +1,38 @@
-def get_token():
-    pass
+from functools import wraps
+
+import jwt
+
+from book_keeping_backend_package import db_utilities
+
+def generate_token(secret):
+
+    encoded_jwt = jwt.encode({"string": "liang shen zui shuai!"}, secret, algorithm='HS256')
+
+    return encoded_jwt
 
 
 def revoke_token():
     pass
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+
+        token = None
+        
+        if 'x-access-tokens' in request.headers:
+         token = request.headers['x-access-tokens']
+
+        if not token:
+            return jsonify({'message': 'missing token'})
+
+        try:
+            user_email = db_utilities.get_redis_db_val(token)
+            print("token found!", token)
+        except:
+            return jsonify({'message': 'token invalid'})
+
+        return f(user_email, *args, **kwargs)
+
+    return decorated_function

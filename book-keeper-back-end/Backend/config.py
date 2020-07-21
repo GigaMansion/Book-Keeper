c9fd58aada84ef1faae28cc0f9e7082ff6087f25
WilsonWang881 <Wilson.Wang@WilsonWang.org>
2020-07-20 21:56:04 -0500

    user can login and submit reimburse

--- book-keeper-back-end/Backend/config.py
+++ book-keeper-back-end/Backend/config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'college-station'
     
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+mysqlconnector://wilson:password@localhost/db_bookkeeper'
+        'mysql+mysqlconnector://wilson:password@book_keeper_db/db_bookkeeper'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     

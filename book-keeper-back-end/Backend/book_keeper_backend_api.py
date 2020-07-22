1c6a24f56ad1e23cdfb6375b1764aaae12c3d49e
WilsonWang881 <Wilson.Wang@WilsonWang.org>
2020-07-22 16:23:58 -0500

    rearranged file structure

--- book-keeper-back-end/Backend/book_keeper_backend_api.py
+++ book-keeper-back-end/Backend/book_keeper_backend_api.py
@@ -1,8 +1,3 @@
-from book_keeping_backend_package import create_app, db
-from book_keeping_backend_package.models import User, Reimburse
+from book_keeping_backend_package import create_app
 
 app = create_app()
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Reimburse': Reimburse}
